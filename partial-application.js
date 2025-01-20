b098b9f7a0a93efb8e947d39e76c0b0f0ac1f559
Jahid Hasan Pallob <62051569+pallobjahid@users.noreply.github.com>
2025-01-20 16:41:44 +0600

    Update partial-application.js

--- partial-application.js
+++ partial-application.js
@@ -1,5 +1,5 @@
 const partialFuction = (a, b) => (c) => a + b + c;
 
-const result = pause(1, 3); // Partial application in one go
+const result = partialFuction(1, 3); // Partial application in one go
 console.log(result(4));
 console.log(result(8));
